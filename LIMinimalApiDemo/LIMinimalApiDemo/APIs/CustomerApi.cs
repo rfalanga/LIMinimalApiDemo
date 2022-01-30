40df2bd9037205df22d042af1bb60dcde31b604d
Rod Falanga <rfalanga@users.noreply.github.com>
2022-01-29 19:04:20 -0700

    following refactorization

---
+++ LIMinimalApiDemo/LIMinimalApiDemo/APIs/CustomerApi.cs
@@ -0,0 +1,29 @@
+﻿using MinimalApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIMinimalApiDemo.APIs
+{
+    public static class CustomerApi
+    {
+        public static void RegisterCustomerAPI(this WebApplication application)
+        {
+            application.MapGet("/customers", GetAllCustomers);
+        }
+
+        private static IResult GetAllCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
+                new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
+                new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
+            };
+
+            return Results.Ok(customers);
+        }
+    }
+}
