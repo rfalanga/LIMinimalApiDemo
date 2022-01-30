2c213131cafa27525b287380cbe3d7b00707f7e1
Rod Falanga <rfalanga@users.noreply.github.com>
2022-01-30 16:12:05 -0700

    still refactoring code

--- LIMinimalApiDemo/LIMinimalApiDemo/APIs/CustomerApi.cs
+++ LIMinimalApiDemo/LIMinimalApiDemo/APIs/CustomerApi.cs
@@ -11,14 +11,14 @@ namespace LIMinimalApiDemo.APIs
 
         public static IResult GetAllCustomers()
         {
-            var customers = new List<Customer>
-            {
-                new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
-                new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
-                new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
-            };
+            //var customers = new List<Customer>
+            //{
+            //    new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
+            //    new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
+            //    new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
+            //};
 
-            return Results.Ok(customers);
+            //return Results.Ok(customers);
         }
     }
 }
---
+++ LIMinimalApiDemo/MinimalApi.DataAccess/CustomerRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MinimalApi.DataAccess.Interfaces;
+using MinimalApi.Model;
+
+namespace MinimalApi.DataAccess
+{
+    public class CustomerRepository : IRepository
+    {
+        private List<Customer> _customerList;
+
+        public CustomerRepository()
+        {
+            _customerList = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
+                new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
+                new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
+            };
+        }
+        public List<Customer> GetAllCustomers()
+        {
+            return _customerList;
+        }
+    }
+}
