56a6f3bf0e7b98ea3c05fb35674e884dd792bb76
Rod Falanga <rfalanga@users.noreply.github.com>
2022-01-29 18:56:04 -0700

    adding a list of customers

--- LIMinimalApiDemo/LIMinimalApiDemo/Program.cs
+++ LIMinimalApiDemo/LIMinimalApiDemo/Program.cs
@@ -1,6 +1,18 @@
+using MinimalApi.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/customers", () =>
+{
+    return new List<Customer>
+    {
+        new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
+        new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
+        new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
+    };
+});
 
 app.Run();
