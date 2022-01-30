e3029aedcf007729d732c423aeabf539df5c6bc8
Rod Falanga <rfalanga@users.noreply.github.com>
2022-01-29 19:08:39 -0700

    introduced the RegisterCustomerAPI

--- LIMinimalApiDemo/LIMinimalApiDemo/Program.cs
+++ LIMinimalApiDemo/LIMinimalApiDemo/Program.cs
@@ -1,18 +1,11 @@
-using MinimalApi.Model;
+using LIMinimalApiDemo.APIs;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/customers", () =>
-{
-    return new List<Customer>
-    {
-        new Customer { Id = 1, FirstName = "George", LastName = "Jones", Age = 70 },
-        new Customer { Id = 2, FirstName = "Bill", LastName = "Smith", Age = 50 },
-        new Customer { Id = 3, FirstName = "Juanita", LastName = "Lopez", Age = 35 }
-    };
-});
+//refactoring into the CustomerApi class
+app.RegisterCustomerAPI();
 
 app.Run();
